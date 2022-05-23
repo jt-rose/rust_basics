c3b76409c424cfbbfaab68a9475173d9e8339b15
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-23 02:17:16 -0400

    add for loop examples

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ fn main() {
     let mut my_name = String::new();
 
     // string interpolation
-    println!("Hello {}!", "Jeff")
+    println!("Hello {}!", "Jeff");
 
     // declare variables
+
+    // types
+    // str and String, similar to Java's primitives vs Wrapper classes
 
     // check types
 
@@ -29,6 +32,22 @@ fn main() {
     // declare Rust equivalent of a class / object
 
     // for loops / iterators
+    let nums = [1,2,3];
+
+    // ---> explicit amount of rounds 0 through 9 (stop at 10)
+    for i in 0..3 {
+        println!("{}", nums[i]);
+    }
+
+    // ---> use length to set up dynamic iteration
+    for i in 0..nums.len() {
+        println!("{}", nums[i]);
+    }
+
+    // ---> use shorthand when index number isn't needed
+    for n in nums {
+        println!("{}", n);
+    }
 
     // functional streams
 
