3485a45090ee253de2cf5d75fec96dda0ae5842b
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-23 01:21:59 -0400

    add note headers

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,19 @@ fn main() {
     // hello world
     println!("Hello, Rust!");
 
+    // declare variables
 
+    // check types
+
+    // check for equality
+
+    // declare function
+
+    // declare Rust equivalent of a class / object
+
+    // functional streams
+
+    // array CRUD
+
+    // ...and more
 }
