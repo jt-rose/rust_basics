866ad48b6998b79a036ee0d3b226e7ac89318079
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-23 02:05:48 -0400

    add if / else sample

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     // hello world
     println!("Hello, Rust!");
 
+    // capture user input
+    let mut my_name = String::new();
+
     // string interpolation
     println!("Hello {}!", "Jeff")
 
@@ -13,11 +16,27 @@ fn main() {
 
     // check for equality
 
+    // if / else
+    let victory = true;
+    if victory == true {
+        println!("Congrats");
+    } else {
+        println!("Oh, too bad...");
+    }
+
     // declare function
 
     // declare Rust equivalent of a class / object
 
+    // for loops / iterators
+
     // functional streams
+
+    // arrays / vectors
+    // ---> arrays, fixed length and store same type
+    let my_numbers = [1,2,3];
+
+
 
     // array CRUD
 
